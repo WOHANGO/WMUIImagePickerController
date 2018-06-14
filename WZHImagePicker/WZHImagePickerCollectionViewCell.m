96f9d06115882a695cbd5deccbf9ab25409e43bb
吳梓杭 <wuzihangjql@qq.com>
2018-06-14 22:54:24 +0800

    封装

---
+++ WZHImagePicker/WZHImagePickerCollectionViewCell.m
@@ -0,0 +1,132 @@
+
+//
+//  WZHImagePickerCollectionViewCell.m
+//  TZImagePickerController
+//
+//  Created by 吳梓杭 on 4/5/18.
+//  Copyright © 2018年 吳梓杭QQ:905640505. All rights reserved.
+//
+
+#import "WZHImagePickerCollectionViewCell.h"
+#import "WZHImageMacro.h"
+
+#define kUIScreenWidth ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? [UIScreen mainScreen].nativeBounds.size.width / [UIScreen mainScreen].nativeScale : [UIScreen mainScreen].bounds.size.width)
+#define kUIScreenHeight ([[UIScreen mainScreen] respondsToSelector:@selector(nativeBounds)] ? [UIScreen mainScreen].nativeBounds.size.height / [UIScreen mainScreen].nativeScale : [UIScreen mainScreen].bounds.size.height)
+
+@implementation WZHImagePickerCollectionViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self imageView];
+        [self videoImageView];
+        [self deleteBtn];
+        [self gifLable];
+    }
+    return self;
+}
+
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView= [[UIImageView alloc] init];
+        _imageView.layer.cornerRadius = 4.f;
+        _imageView.layer.borderWidth = 1.5f;
+        _imageView.layer.borderColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.3].CGColor;
+        _imageView.contentMode = UIViewContentModeScaleAspectFit;
+        [self addSubview:_imageView];
+        self.clipsToBounds = YES;
+        [_imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self).offset(7);
+            make.right.equalTo(self).offset(-7);
+            make.left.bottom.equalTo(self);
+        }];
+    }
+    return _imageView;
+}
+- (UIImageView *)videoImageView {
+    if (!_videoImageView) {
+        _videoImageView = [[UIImageView alloc] init];
+        _videoImageView.image = [UIImage imageNamedFromMyBundle:@"MMVideoPreviewPlay"];
+        _videoImageView.contentMode = UIViewContentModeScaleAspectFill;
+        _videoImageView.hidden = YES;
+        [self addSubview:_videoImageView];
+        [_videoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.center.equalTo(self);
+            make.size.mas_equalTo(CGSizeMake(40, 40));
+        }];
+    }
+    return _videoImageView;
+}
+- (UIButton *)deleteBtn {
+    if (!_deleteBtn) {
+        _deleteBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_deleteBtn setImage:[UIImage imageNamed:@"To_choose"] forState:UIControlStateNormal];
+        _deleteBtn.imageEdgeInsets = UIEdgeInsetsMake(-17, 0, 0, -17);
+        _deleteBtn.alpha = 0.6;
+        [self addSubview:_deleteBtn];
+        [_deleteBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.right.equalTo(self);
+            make.size.mas_equalTo(CGSizeMake(36, 36));
+        }];
+    }
+    return _deleteBtn;
+}
+- (UILabel *)gifLable {
+    if (!_gifLable) {
+        [self layoutIfNeeded];
+        _gifLable = [[UILabel alloc] initWithFrame:CGRectMake(0, self.frame.size.height - 14, self.imageView.frame.size.width, 14)];
+        _gifLable.text = @"GIF";
+        _gifLable.textColor = [UIColor whiteColor];
+        _gifLable.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.8];
+        _gifLable.textAlignment = NSTextAlignmentCenter;
+        _gifLable.font = [UIFont systemFontOfSize:10];
+        UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:_gifLable.bounds byRoundingCorners:UIRectCornerBottomLeft | UIRectCornerBottomRight cornerRadii:CGSizeMake(4, 4)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        maskLayer.frame = _gifLable.bounds;
+        maskLayer.path = maskPath.CGPath;
+        _gifLable.layer.mask  = maskLayer;
+        [self addSubview:_gifLable];
+        
+    }
+    return _gifLable;
+}
+
+- (void)setAsset:(id)asset {
+    _asset = asset;
+    if ([asset isKindOfClass:[PHAsset class]]) {
+        PHAsset *phAsset = asset;
+        _videoImageView.hidden = phAsset.mediaType != PHAssetMediaTypeVideo;
+        _gifLable.hidden = ![[phAsset valueForKey:@"filename"] tz_containsString:@"GIF"];
+    } else if ([asset isKindOfClass:[ALAsset class]]) {
+        ALAsset *alAsset = asset;
+        _videoImageView.hidden = ![[alAsset valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypeVideo];
+        _gifLable.hidden = YES;
+    }
+}
+
+- (void)setRow:(NSInteger)row {
+    _row = row;
+    _deleteBtn.tag = row;
+}
+
+- (UIView *)snapshotView {
+    UIView *snapshotView = [[UIView alloc]init];
+    UIView *cellSnapshotView = nil;
+    
+    CGSize size = CGSizeMake((kUIScreenWidth - 50) / 4, (kUIScreenWidth - 50) / 4);
+    UIGraphicsBeginImageContextWithOptions(size, self.opaque, 0);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage * cellSnapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    cellSnapshotView = [[UIImageView alloc]initWithImage:cellSnapshotImage];
+
+    snapshotView.frame = CGRectMake(0, 0, cellSnapshotView.frame.size.width, cellSnapshotView.frame.size.height);
+    cellSnapshotView.frame = CGRectMake(0, 0, cellSnapshotView.frame.size.width, cellSnapshotView.frame.size.height);
+    cellSnapshotView.layer.cornerRadius = 4.f;
+    cellSnapshotView.layer.masksToBounds = YES;
+    
+    [snapshotView addSubview:cellSnapshotView];
+    return snapshotView;
+}
+
+
+@end
---
+++ WZHImagePicker/WZHImagePickerController.h
@@ -0,0 +1,127 @@
+//
+//  WZHImagePickerController.h
+//  TZImagePickerController
+//
+//  Created by 吳梓杭 on 4/5/18.
+//  Copyright © 2018年 吳梓杭QQ:905640505. All rights reserved.
+//
+/**
+ 
+ 话说在前，此demo使用https://github.com/banchichen/TZImagePickerController进行优化封装
+ 如谭真同志想弄我，那就弄吧😂😂😂😂
+ 
+ 作者谭真代码修改部分如下：
+ "LxGridViewFlowLayout.h"      18-21行
+ "LxGridViewFlowLayout.m"      80-96行
+ 
+ 此封装含有Masonry，请自行修改
+ 
+ */
+
+#import <UIKit/UIKit.h>
+
+/**
+ ImagePicker类型
+ 
+ - WZHImagePickerTypeDefault: 默认样式，不创建列表
+ - WZHImagePickerTypeTakePhoto: 相机
+ - WZHImagePickerTypeAlbum: 相册
+ - WZHImagePickerTypeCollectionView: collcetionView样式
+ - WZHImagePickerTypelongPressGestureCollectionView: collcetionView样式,含有长按
+ */
+typedef NS_ENUM(NSInteger, WZHImagePickerType) {
+    WZHImagePickerTypeDefault,
+    WZHImagePickerTypeTakePhoto,
+    WZHImagePickerTypeAlbum,
+    WZHImagePickerTypeCollectionView,
+    WZHImagePickerTypelongPressGestureCollectionView,
+};
+
+/**
+ collectionView选择回调
+
+ @param collectionPhotos collectionView选择回调
+ */
+typedef void(^myCollectionViewPickerBlock)(NSArray<UIImage *> *collectionPhotos);
+
+/**
+ 相册下选择照片回调
+ 
+ @param photos 相册下选择照片回调
+ */
+typedef void(^myAlbumsPickerBlock)(NSArray<UIImage *> *photos);
+
+/**
+ 拍照回调
+ 
+ @param cropImage 拍照回调
+ */
+typedef void(^myPhotographBlock)(UIImage *cropImage);
+
+@interface WZHImagePickerController : UIView
+
+/**
+ collcetionView下选择回调
+ */
+@property (nonatomic, strong) myCollectionViewPickerBlock collectionBlock;
+/**
+ 非collectionView相册下选择照片回调
+ */
+@property (nonatomic, strong) myAlbumsPickerBlock albumsBlock;
+/**
+ 非collectionView拍照回调
+ */
+@property (nonatomic, strong) myPhotographBlock photographBlock;
+/**
+ 数量，默认1张
+ */
+@property (nonatomic, assign) NSInteger maxCount;
+/**
+ 内部显示拍照按钮，默认NO
+ */
+@property (nonatomic, assign) BOOL insideTakePhoto;
+/**
+ 内部显示拍视频按钮，默认NO
+ */
+@property (nonatomic, assign) BOOL insideTakeVideo;
+/**
+ 时间升降序，默认YES
+ */
+@property (nonatomic, assign) BOOL sortAscending;
+/**
+ 是否允许选择视频，默认NO
+ */
+@property (nonatomic, assign) BOOL allowPickingVideo;
+/**
+ 是否允许选择图片，默认YES
+ */
+@property (nonatomic, assign) BOOL allowPickingImage;
+/**
+ 是否允许选择GIF，默认NO
+ */
+@property (nonatomic, assign) BOOL allowPickingGif;
+/**
+ 是否选择原图，默认YES
+ */
+@property (nonatomic, assign) BOOL allowPickingOriginalPhoto;
+/**
+ 是否多选GIF或视频，默认YES
+ */
+@property (nonatomic, assign) BOOL allowPickingMuitlpleGifOrVideo;
+/**
+ 是否允许裁剪，默认NO
+ */
+@property (nonatomic, assign) BOOL allowCrop;
+/**
+ 是否圆形裁剪，默认NO
+ */
+@property (nonatomic, assign) BOOL needCircleCrop;
+/**
+ 裁剪直径，默认[UIScreen mainScreen].bounds.size.width
+ */
+@property (nonatomic, assign) float diameter;
+
+
+- (instancetype)initWithType:(NSInteger)type;
+
+@end
